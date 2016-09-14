5403c7cdb7534a9dbc85c71a632a44c51eedee58
AtomJump <peter@atomjump.com>
2016-09-14 14:22:07 +1200

    checking

--- insert.php
+++ insert.php
@@ -22,6 +22,7 @@
 	if($staging == 1) {
 		$staging = true;
 	}
+	error_log("Insert emoticons path:" . $start_path);
 	include_once($start_path . 'config/db_connect.php');	
 	
     $define_classes_path = $start_path;     //This flag ensures we have access to the typical classes, before the cls.pluginapi.php is included
