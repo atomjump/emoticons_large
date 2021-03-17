97b12df95233be35c446096d4c929a709cda2b9d
AtomJump <peter@atomjump.com>
2021-03-18 09:17:00 +1300

    update server functionality

--- view-folder.php
+++ view-folder.php
@@ -26,6 +26,7 @@
 	$start_path = add_trailing_slash_local($emoticons_large_config['serverPath']);
 	$notify = true;		//this switches on notifications from this message
 	$staging = $emoticons_large_config['staging'];
+	error_log("Staging: " . $staging);		//TESTING
 	if($staging == 1) {
 		$staging = true;
 	}
