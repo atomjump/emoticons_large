10df9ee6dbd9098e5814ee33b5d342a3fe40b7ae
AtomJump <peter@atomjump.com>
2021-03-17 15:19:22 +1300

    more folders

---
+++ view-folder.php
@@ -0,0 +1,40 @@
+	function trim_trailing_slash_local($str) {
+        return rtrim($str, "/");
+    }
+    
+    function add_trailing_slash_local($str) {
+        //Remove and then add
+        return rtrim($str, "/") . '/';
+    }
+
+    if(!isset($emoticons_large_config)) {
+        //Get global plugin config - but only once
+        $data = file_get_contents (dirname(__FILE__) . "/config/config.json");
+        if($data) {
+            $emoticons_large_config = json_decode($data, true);
+            if(!isset($emoticons_large_config)) {
+                echo "Error: emoticons_large config/config.json is not valid JSON.";
+                exit(0);
+            }
+        } else {
+            echo "Error: Missing config/config.json in emoticons_large plugin.";
+            exit(0);
+        }
+    }
+
+	$start_path = add_trailing_slash_local($emoticons_large_config['serverPath']);
+	$notify = true;		//this switches on notifications from this message
+	$staging = $emoticons_large_config['staging'];
+	if($staging == 1) {
+		$staging = true;
+	}
+	include_once($start_path . 'config/db_connect.php');	
+	
+    $define_classes_path = $start_path;     //This flag ensures we have access to the typical classes, before the cls.pluginapi.php is included
+	require($start_path . "classes/cls.pluginapi.php");
+
+    $api = new cls_plugin_api();
+
+	global $root_server_url;
+	global $local_server_path;
+	
